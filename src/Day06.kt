94da62462310a0e7f43a155124b8e1c29135685b
Alexander Voß <alexander.voss@it-voss.com>
2023-12-06 08:30:12 +0100

    refactored with shrinkIf

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,12 +3,15 @@
 
 // solutions part 1: 1413720, part 2: 30565288
 // 288 / 71503
+
+private fun String.shrinkIf(predicate: Boolean) = if (predicate) replace(" ","") else this
+
 fun main() {
     sequenceOf(filename = "Day06.txt").toList().also { (timesStr,distStr) ->
         print("06.12.23 AoC |")
         for (puzzle in 1..2) {
-            val times = timesStr.split(":")[1].let { if (puzzle==2) it.replace(" ","") else it }.toLongList()
-            val dists = distStr.split(":")[1].let { if (puzzle==2) it.replace(" ","") else it }.toLongList()
+            val times = timesStr.split(":")[1].shrinkIf(puzzle==2).toLongList()
+            val dists = distStr.split(":")[1].shrinkIf(puzzle==2).toLongList()
 
             val ways = times.zip(dists).fold(1) { rc, (time,dist) -> rc * (0..time).count { dt -> (time-dt)*dt > dist } }
             print(" part $puzzle: $ways")
