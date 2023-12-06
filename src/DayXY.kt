be14d4d47c71a190b46ffa11a17ac2fa3b5c26d8
Alexander Voß <alexander.voss@it-voss.com>
2023-12-06 15:03:47 +0100

    template added

---
+++ src/DayXY.kt
@@ -0,0 +1,11 @@
+// (C) 2023 A.Voß, a.voss@fh-aachen.de, kotlin@codebasedlearning.dev
+// for advent of code 2023
+
+// solutions part 1: x, part 2: y
+// example x / y
+fun main() {
+    sequenceOf(filename = "DayXYa.txt").forEach { line ->
+        println("line '$line'")
+    }
+    println("XY.12.23 AoC | part 1: x, part 2: y")
+}
